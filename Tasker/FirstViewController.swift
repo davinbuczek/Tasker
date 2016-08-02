db2f072ae1402b100634ac0f038f054640d272e4
davinbuczek <davinbuczek@me.com>
2016-08-02 15:22:26 -0700

    Initial Commit

---
+++ Tasker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tasker
+//
+//  Created by Davin Buczek on 8/2/16.
+//  Copyright © 2016 Davin Buczek. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
