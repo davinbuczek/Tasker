2c9363098f57e9fe52d8649c1528cf3eb83296d5
davinbuczek <davinbuczek@me.com>
2016-08-02 16:58:40 -0700

    Tasker
    A simple To-do list app written in Swift

--- Tasker/SecondViewController.swift
+++ Tasker/SecondViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var txtTask: UITextField!
+    @IBOutlet var txtDesc: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,27 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //Events
+    @IBAction func btnAddTask_Click(sender: UIButton) {
+        taskMgr.addTask(txtTask.text!, desc: txtDesc.text!)
+        self.view.endEditing(true)
+        txtTask.text = ""
+        txtDesc.text = ""
+        self.tabBarController?.selectedIndex = 0;
+    }
 
-
+    //Touch Functions
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    //UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder();
+        
+        return true
+    }
 }
 
